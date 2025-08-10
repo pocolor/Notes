faacdca312388cc9b4ff7ae574ab72c665a74447
poco <luvecjr@gmail.com>
2025-08-10 21:40:12 +0200

    replacing JavaFX by Swing

--- src/main/java/com/pocolor/notes/startup/controller/template/TemplateController.java
+++
@@ -1,5 +0,0 @@
-package com.pocolor.notes.startup.controller.template;
-
-public class TemplateController {
-    public static final String TEMPLATE_FXML_RESOURCE_PATH = "/fxml/startup/template/Template.fxml";
-}
